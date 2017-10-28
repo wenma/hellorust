7ccc07e95885c7b1f8c1065ee19ef6e4d7d9bc51
wen.ma <wen.ma@qunar.com>
2017-10-28 14:04:30 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,34 @@
 
-// rust 是一个面向表达式的语言
+/*
+//---------------------------------------------------
+// 变量
+
+fn main() {
+    let x = 5;
+    println!("{}", x);
+
+    let y:i32 = 7;
+    println!("{}", y);
+
+    assert_eq!(y, 7);
+    println!("Hello, world!");
+
+    let (a, b): (bool,bool) = (true, false);
+    println!("{:?} {:?}", a, b);
+
+    let c: &str = "hello world";
+    let d: String = c.to_string();
+    println!("{:?}{:?}", c, d);
+
+} 
+*/
 
 
-// fn main() {
-//     // 变量绑定
-//     let x = 5;
-//     println!("{}", x);
+/*
+//---------------------------------------------------
+// 一个猜数游戏
 
-//     let y:i32 = 7;
-//     println!("{}", y);
-
-//     assert_eq!(y, 7);
-//     println!("Hello, world!");
-
-//     let (a, b): (bool,bool) = (true, false);
-//     println!("{:?} {:?}", a, b);
-
-//     let c: &str = "qqqqqqqqqq";
-//     let d: String = c.to_string();
-//     println!("{:?}{:?}", c, d);
-
-// }
-
-extern crate rand;    // 使用外部包
+extern crate rand;
 
 use std::io;
 use rand::Rng;
@@ -54,8 +60,70 @@ fn main() {
     }
     
 }
+*/
 
 
+/*
+//---------------------------------------------------
+// 函数
+
+fn add(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+fn main() {
+    println!("{}", add(1, 2))
+}
+*/
+
+
+/*
+//---------------------------------------------------
+// 发散函数
+
+fn p() -> ! {
+    panic!("Error on something");
+}
+
+fn main() {
+    p();
+}
+*/
+
+
+/*
+//---------------------------------------------------
+// 函数指针
+
+fn add(i: i32) -> i32 {
+    i + 1
+}
+
+fn main() {
+    let f: fn(i32) -> i32 = add;
+    println!("{}", f(1));
+}
+*/
+
+
+/*
+//---------------------------------------------------
+// 数组
+
+fn main() {
+    let a = [1, 2, 3];
+    println!("{:?}", a.len());
+
+    let b: [u8; 3] = [1, 2, 3];
+    println!("{:?}", b.len());
+
+    let c = [0; 20];
+    println!("{:?}", c.len());
+
+    let d: [&str; 3] = ["hello", "world", "rust"];
+    println!("{:?}", d[2]); 
+}
+*/
 
 
 
