a5d914310c91865892e72ccc06b9e4f416a1f340
wen.ma <wen.ma@qunar.com>
2017-10-31 21:20:37 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1011,61 +1011,51 @@ fn main() {
 */
 
 
+
+/*
+//---------------------------------------------------
+// 指针
+
 fn main() {
-    // Assign a reference of type `i32`. The `&` signifies there
-    // is a reference being assigned.
+
     let reference = &4;
 
     match reference {
-        // If `reference`s is pattern matched against `&val`, it results
-        // in a comparison like:
-        // `&i32`
-        // `&val`
-        // ^ We see that if the matching `&`s are dropped, then the `i32`
-        // should be assigned to `val`.
-        &val => println!("Got a value via destructuring: {:?}", val),
+        ref val => println!("Got a value via destructuring: {:p}", val),
     }
 
-    // To avoid the `&`, you dereference before matching.
     match *reference {
         val => println!("Got a value via dereferencing: {:?}", val),
     }
 
-    // What if you don't start with a reference? `reference` was a `&`
-    // because the right side was already a reference. This is not
-    // a reference because the right side is not one.
     let _not_a_reference = 3;
 
-    // Rust provides `ref` for exacty this purpose. It modifies the
-    // assignment so that a reference is created for the element; this
-    // reference is assigned.
     let ref _is_a_reference = 3;
 
-    // Accordingly, by defining 2 values without references, references
-    // can be retrieved via `ref` and `ref mut`.
+
     let value = 5;
     let mut mut_value = 6;
 
-    // Use `ref` keyword to create a reference.
     match value {
-        ref r => println!("Got a reference to a value: {:?}", r),
+        ref r => println!("Got a reference to a value: {:p}", r),
     }
 
-    // Use `ref mut` similarly.
     match mut_value {
         ref mut m => {
-            // Got a reference. Gotta dereference it before we can
-            // add anything to it.
             *m += 10;
             println!("We added 10. `mut_value`: {:?}", m);
         },
     }
-
-
 
     let a = &1;
     let b = &2;
     let c = &3;
 
     println!("{:?}", *a + *b + *c);
+}
+*/
+
+
+fn main() {
+    
 }
